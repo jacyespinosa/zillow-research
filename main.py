5747b939da69426d1948f4166caf26b8141fd102
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-08-07 22:43:45 -0700

    Access the link list and enter each link in the form field. Once all the fields are filled, click submit.

--- main.py
+++ main.py
@@ -35,3 +35,18 @@ while not over:
         price_question.click()
         time.sleep(1)
         price_question.send_keys(f'{price[i]}')
+
+        link_question = driver.find_element_by_xpath(
+            '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        link_question.click()
+        time.sleep(1)
+        link_question.send_keys(f'{links[i]}')
+
+        submit = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+        submit.click()
+
+        time.sleep(3)
+        submit_another = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+        submit_another.click()
+
+    over = True
